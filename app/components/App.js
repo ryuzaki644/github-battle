97fc4acf18ea8e5cdc3612d90ae4deb77f1e43c0
ryuzaki644 <sathwikkoushal@gmail.com>
2017-06-21 08:51:12 +0530

    Add component App, return 'Hello World!'

---
+++ app/components/App.js
@@ -0,0 +1,11 @@
+const React = require('react')
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>Hello World!</div>
+    )
+  }
+}
+
+module.exports = App
---
+++ app/index.js
@@ -0,0 +1,6 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+require('./index.css')
+const App = require('./components/App.js')
+
+ReactDOM.render(<App />, document.getElementById('app'))
