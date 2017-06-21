4564ec261af36fcfdaadae2e779e893699acb873
ryuzaki644 <sathwikkoushal@gmail.com>
2017-06-21 10:01:03 +0530

    Modify App component   -require and render Popular component

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,11 @@
 const React = require('react')
-
+const Popular = require('./Popular')
 class App extends React.Component {
   render () {
     return (
-      <div>Hello World!</div>
+      <div>
+            <Popular />
+            </div>
     )
   }
 }
