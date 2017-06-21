19b39090340b7b233c5fc7c0c4b50d1af0337ba2
ryuzaki644 <sathwikkoushal@gmail.com>
2017-06-21 23:13:58 +0530

    Add react-router-dom for routing to nav-link paths   -Add react-router-dom to dependencies in package.json   -Add Nav function to render list of navigation links in Nav.js   -Add Router to render navigation links and other components in App.js   -Add Route to render a component for a selected path in App.js   -Add flex-box properties to Nav in index.css

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,19 @@
 const React = require('react')
+const ReactRouter = require('react-router-dom')
 const Popular = require('./Popular')
+const Router = ReactRouter.BrowserRouter
+const Route = ReactRouter.Route
+const Nav = require('./Nav')
+
 class App extends React.Component {
   render () {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <Nav />
+          <Route path='/popular' component={Popular} />
+        </div>
+      </Router>
     )
   }
 }
