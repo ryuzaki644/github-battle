a9c0ecab02cf24a1121aa5cfe89ec70f8ed3fbb0
ryuzaki644 <sathwikkoushal@gmail.com>
2017-06-21 12:38:36 +0530

    Modify Popular to contain modularized components   -Add SelectLanguage to render the languages   -Add SelectLanguage component to Popular   -Add propTypes to validate prop-types being passed   -Add propTypes to validate required prop-types are being passed   -Add props to SelectLanguage component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,29 @@
 const React = require('react')
+const propTypes = require('prop-types')
 
+class SelectLanguage extends React.Component {
+  render () {
+    const languages = ['All', 'Javascript', 'Python', 'clojure', 'Haskell']
+    return (
+      <ul className='languages'>
+        {languages.map(function (lang) {
+          return (
+            <li
+              style={lang === this.props.selectedLanguage ? {color: 'teal'} : null}
+              onClick={this.props.onSelect.bind(null, lang)}
+              key={lang}>
+              {lang}
+            </li>)
+        }, this)}
+      </ul>
+    )
+  }
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: propTypes.string.isRequired,
+  onSelect: propTypes.func.isRequired
+}
 class Popular extends React.Component {
   constructor (props) {
     super(props)
@@ -17,19 +41,12 @@ class Popular extends React.Component {
   }
 
   render () {
-    const languages = ['All', 'Javascript', 'Python', 'clojure', 'Haskell']
     return (
-      <ul className='languages'>
-        {languages.map(function (lang) {
-          return (
-            <li
-              style={lang === this.state.selectedLanguage ? {color: 'teal'} : null}
-              onClick={this.updateLanguage.bind(null, lang)}
-              key={lang}>
-              {lang}
-            </li>)
-        }, this)}
-      </ul>
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage} />
+      </div>
     )
   }
 }
